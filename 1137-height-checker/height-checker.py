1b61dbdff6cf4dc15484aa64f62cc0755ad5f490
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-22 01:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.03%) - LeetSync

---
+++ 1137-height-checker/height-checker.py
@@ -0,0 +1,9 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        exp=sorted(heights)
+        c=0
+        for i in range(len(heights)):
+            if heights[i]!=exp[i]:
+                c+=1
+        return c
+        
