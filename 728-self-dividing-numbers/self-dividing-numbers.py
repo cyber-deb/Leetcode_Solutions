d76face1ebf6854f70b1130b4a82f2be892411cf
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-06-21 19:03:03 +0530

    Time: 7 ms (58.84%) | Memory: 19.3 MB (36.58%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.py
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -1,17 +1,16 @@
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        l=[]
-        c=0
-        for i in range (left,right+1):
-            if len(str(i))==1:
-                l.append(i)
-            elif '0' not in set(str(i)):
-                for j in list(str(i)):
-                    if i%int(j)!=0:
-                        break
-                    c+=1
-                if c==len(str(i)):
-                    l.append(i)
-                c=0
-        return l
+        ans=[]
+        for num in range(left,right+1):
+            t=num
+            valid=True
+            while t>0:
+                digit=t%10
+                if digit==0 or num%digit!=0:
+                    valid=False
+                    break
+                t//=10
+            if valid:
+                ans.append(num)
+        return ans
                 
