b90f03c9794dd2f3b3bdea8e52f8343f7d4bd739
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-07-06 01:04:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (45.75%) - LeetSync

--- 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.py
+++ 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.py
@@ -1,11 +1,5 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        k=max(nums)
-        c=0
-        for i in nums:
-            while i!=k:
-                i+=1
-                c+=1
-        return c
+        return max(nums)*len(nums)-sum(nums)
 
         
