03a034ba5bd2d67f415874277f71411334158d93
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-07-10 23:52:07 +0530

    Time: 47 ms (24.46%) | Memory: 19.2 MB (86.55%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -1,9 +1,7 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
         c=0
-        while True:
-            if num1==0 or num2==0:
-                break
+        while num1!=0 and num2!=0:
             if num1>num2:
                 num1-=num2
             else:
