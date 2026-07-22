a1a830fb1c351b241f2faf5beb7e0dd039e7b06e
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-07-23 01:18:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.13%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        c=0
+        for i in logs:
+            if i=="../":
+                if c==0:
+                    c+=0
+                else:
+                    c-=1
+            elif i=="./":
+                c+=0
+            else:
+                c+=1
+        return c
+        
