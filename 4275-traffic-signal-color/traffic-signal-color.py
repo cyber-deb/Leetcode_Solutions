0456b5aaed8f5ccc199aafb1af3e7f742e07b927
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-07-22 21:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (83.44%) - LeetSync

---
+++ 4275-traffic-signal-color/traffic-signal-color.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trafficSignal(self, timer: int) -> str:
+        if 30<timer<=90:
+            return "Red"
+        elif timer==30:
+            return "Orange"
+        elif timer==0:
+            return "Green"
+        else:
+            return "Invalid"
+        
