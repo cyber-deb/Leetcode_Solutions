011f54123c83cf1f2f8e03bd9328e10edc4a074e
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-07-28 22:57:46 +0530

    Time: 16 ms (28.68%) | Memory: 28.5 MB (18.99%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def inorder(self, root):
+        ans = []
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            ans.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return ans
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: Optional[TreeNode]
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        c=0
+        x=self.inorder(root)
+        for y in x:
+            if low<=y<=high:
+                c+=y
+        return c
+        def rangeSumBST(self, root, low, high):
+            c=0
+            x=self.inorder(root)
+            for y in x:
+                if low<=y<=high:
+                    c+=y
+            return c
