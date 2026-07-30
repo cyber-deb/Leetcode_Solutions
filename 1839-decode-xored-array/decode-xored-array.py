e45a168d67d86a00ce75e9436acbdac8beffb0fc
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-07-30 22:08:05 +0530

    Time: 8 ms (19.38%) | Memory: 20.6 MB (89.81%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        arr=[first]
+        for x in encoded:
+            arr.append(arr[-1]^x)
+        return arr
+        
+        
