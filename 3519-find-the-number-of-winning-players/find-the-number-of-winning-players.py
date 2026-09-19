df04e9e8208fd863463aba1d5510f7fa3f188535
Debajyoti Pal <paldebajyoti2000@gmail.com>
2026-09-19 12:40:03 +0530

    Time: 2 ms (89.51%) | Memory: 19.2 MB (96.91%) - LeetSync

--- 3519-find-the-number-of-winning-players/find-the-number-of-winning-players.py
+++ 3519-find-the-number-of-winning-players/find-the-number-of-winning-players.py
@@ -3,10 +3,10 @@ class Solution:
         count = [[0] * 11 for _ in range(n)]
         for player, color in pick:
             count[player][color] += 1
-        ans = 0
+        ans=0
         for player in range(n):
             for color in range(11):
-                if count[player][color] > player:
-                    ans += 1
+                if count[player][color]>player:
+                    ans+=1
                     break
         return ans
